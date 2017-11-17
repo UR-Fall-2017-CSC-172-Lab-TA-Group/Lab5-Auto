81fc6d7a6dde0adbadb9d2e37647120fa760b561
Sifan YE <sye8@u.rochester.edu>
2017-11-17 10:23:52 -0500

    Added test for addAll

--- JUnitTest/URArrayListTest.java
+++ JUnitTest/URArrayListTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -35,11 +38,11 @@ private URArrayList<Integer> testArrList;
 	@Test
 	public void testAddElemWithItr(){
 		System.out.println("Test add 3, 6, 5, 1, 4 and print with iterator");
-		int[] testElem = new int[]{3, 6, 5, 1, 4};
-		String testOut = "";
+		int[] testElem = new int[]{3, 6, 5, 1, 4};		
 		for(int i: testElem){
 			testArrList.add(i);
 		}
+		String testOut = "";
 		for(int i: testArrList){
 			testOut += (i + " ");
 		}
@@ -53,9 +56,48 @@ private URArrayList<Integer> testArrList;
 		assertFalse(testArrList.isEmpty());
 	}
 	
+	@Test
+	public void testAddAll(){
+		System.out.println("Test add 3, 6, 5, 1, 4 as a list and print with iterator");
+		List<Integer> test = new ArrayList<Integer>();
+		test.add(3);
+		test.add(6);
+		test.add(5);
+		test.add(1);
+		test.add(4);
+		testArrList.addAll(test);
+		String testOut = "";
+		for(int i: testArrList){
+			testOut += (i + " ");
+		}
+		assertEquals("3 6 5 1 4 ", testOut);
+	}
+	
+	@Test
+	public void testAllAllAtIndex(){
+		System.out.println("Test add 3, 6, 5, 1, 4 as a list");
+		System.out.println("At index 2 of (4,9,11,15), then print with iterator");
+		List<Integer> test = new ArrayList<Integer>();
+		test.add(3);
+		test.add(6);
+		test.add(5);
+		test.add(1);
+		test.add(4);
+		testArrList.add(4);
+		testArrList.add(9);
+		testArrList.add(11);
+		testArrList.add(15);
+		testArrList.addAll(2, test);
+		String testOut = "";
+		for(int i: testArrList){
+			testOut += (i + " ");
+		}
+		assertEquals("4 9 3 6 5 1 4 11 15 ", testOut);
+	}
+	
 	@After
 	public void runOnceAfterEachTestMethod(){	
-		System.out.println("Run after each test method");
+		System.out.println("Run after each test method\n");
 	}
 	
 	@AfterClass
