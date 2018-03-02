c18f2df0c9bc7c98313e9adb05513803fd3c1b60
Sifan YE <sye8@u.rochester.edu>
2018-03-01 23:04:43 -0500

    Shell script for UNIX

---
+++ JUnitTest/autograde.sh
@@ -0,0 +1,6 @@
+javac -cp .:junit-4.12.jar URArrayListTest.java URLinkedListTest.java
+java -cp .:junit-4.12.jar:java-hamcrest-2.0.0.0.jar org.junit.runner.JUnitCore URArrayListTest > arraylist.txt
+echo "ArrayList test result saved to arraylist.txt"
+java -cp .:junit-4.12.jar:java-hamcrest-2.0.0.0.jar org.junit.runner.JUnitCore URLinkedListTest > linkedlist.txt
+echo "ArrayList test result saved to linkedlist.txt"
+
