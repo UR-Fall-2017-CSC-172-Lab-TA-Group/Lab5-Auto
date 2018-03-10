a0e843b30bcb41722f809a7eb68463a9fbed61cf
Sifan YE <sye8@u.rochester.edu>
2018-03-09 19:22:42 -0500

    Update auto grade to remove .class after test

--- JUnitTest/autograde.sh
+++ JUnitTest/autograde.sh
@@ -3,4 +3,4 @@ java -cp .:junit-4.12.jar:java-hamcrest-2.0.0.0.jar org.junit.runner.JUnitCore U
 echo "ArrayList test result saved to arraylist.txt"
 java -cp .:junit-4.12.jar:java-hamcrest-2.0.0.0.jar org.junit.runner.JUnitCore URLinkedListTest > linkedlist.txt
 echo "ArrayList test result saved to linkedlist.txt"
-
+rm *.class
